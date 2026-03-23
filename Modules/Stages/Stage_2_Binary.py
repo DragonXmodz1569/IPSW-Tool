b9d1f5855a0a21edbcaaf72b31794aae988b24d1
DragonXmodz1569 <Bradleymcm1569@outlook.com>
2026-03-23 00:32:51 +0000

    Added Compare Added Symbols, Starts, Strings and imports txt files added choice between all dyld files or change (mix between efficiency and storage space)
    still need to decompile functions using either ghidra or radare2
    
    then need to work on gui support

--- Modules/Stages/Stage_2_Binary.py
+++ Modules/Stages/Stage_2_Binary.py
@@ -1,5 +1,6 @@
 import json
 import os
+import subprocess
 
 
 class Binary_Compare:
@@ -97,6 +98,11 @@ class Binary_Compare:
 
         get_dylibs(Old, old_map)
         get_dylibs(New, new_map)
+        if os.path.exists(
+                os.path.join(self.Extract_Path, IPSW_Mapping[1]['IPSW Name'].strip('_Dyld_Mapping.json'), 'Extra',
+                             'IPSW_Changes.json')):
+            os.remove(os.path.join(self.Extract_Path, IPSW_Mapping[1]['IPSW Name'].strip('_Dyld_Mapping.json'), 'Extra',
+                                   'IPSW_Changes.json'))
 
         for path in new_map:
             if path not in old_map:
@@ -106,8 +112,15 @@ class Binary_Compare:
 
             elif new_map[path] != old_map[path]:
                 self.Console_Print("CHANGED:", path, old_map[path], "->", new_map[path])
-                with open(os.path.join(self.Extract_Path, IPSW_Mapping[1]['IPSW Name'].strip('_Dyld_Mapping.json'), 'Extra', 'IPSW_Changes.json'), 'a+') as f:
-                    json.dump(("CHANGED:", path, old_map[path], "->", new_map[path]), f, indent=4)
+                with open(os.path.join(self.Extract_Path, IPSW_Mapping[1]['IPSW Name'].strip('_Dyld_Mapping.json'), 'Extra', 'IPSW_Changes.json'), 'a') as f:
+                    new_change = {
+                        "type": "CHANGED",
+                        "path": path,
+                        "old_size": old_map[path],
+                        "new_size": new_map[path]
+                    }
+                    json.dump(new_change, f)
+                    f.write('\n')
 
         for path in old_map:
             if path not in new_map:
@@ -115,7 +128,110 @@ class Binary_Compare:
                 with open(os.path.join(self.Extract_Path, IPSW_Mapping[1]['IPSW Name'].strip('_Dyld_Mapping.json'), 'Extra', 'IPSW_Removed.json'), 'a+') as f:
                     json.dump(("REMOVED:", path), f, indent=4)
 
+    def IPSW_Extra(self, IOS=None, Target='All', Target_Compare_IOS=None):
+        IOS_Targets = []
+        Target_Dyld = []
+        if IOS is not None:
+            for root_folders in os.listdir(self.Extract_Path):
+                if root_folders.split("_")[0] == IOS:
+                    IOS_Targets.append(root_folders)
+            if len(IOS_Targets) == 0:
+                self.Console_Print('[Binary] No IOS Targets Found Extracted')
+                return
+        if IOS is None:
+            self.Console_Print('[Binary] No IOS was Given Running on all')
+            for x in range(len(self.json_mappings)):
+                IOS_Targets.append(self.json_mappings[x].split('/')[-1].strip('_Dyld_Mapping.json'))
+        self.Console_Print('-------------------------------------------------------')
 
+        def extract_file_paths(data):
+            paths = []
+            for item in data:
+                if item.get("Type") == "File":
+                    paths.append(item.get("Path"))
+                if "Children" in item:
+                    paths.extend(extract_file_paths(item["Children"]))
+            return paths
 
+        Dyld_Cache = None
+        for Main_Root in IOS_Targets:
+            for Root_Folders in os.listdir(os.path.join(self.Extract_Path, Main_Root)):
+                if Root_Folders.endswith('.dmg'):
+                    if not os.path.exists(os.path.join(self.Extract_Path, Main_Root, 'Extra', Root_Folders.replace('.dmg', ''))):
+                        continue
+                    for sub_root in os.listdir(os.path.join(self.Extract_Path, Main_Root, 'Extra', Root_Folders.replace('.dmg', ''))):
+                        if not os.path.exists(os.path.join(self.Extract_Path, Main_Root, 'Extra', Root_Folders.replace('.dmg', ''), sub_root, 'System', 'Library', 'Caches', 'com.apple.dyld')):
+                            continue
+                        Dyld_Cache = os.path.join(os.path.join(self.Extract_Path, Main_Root, 'Extra', Root_Folders.replace('.dmg', ''), sub_root))
 
+        for Root_Folders in IOS_Targets:
+            if Target.lower() == 'all':
+                for dyld_map in os.listdir(os.path.join(self.Extract_Path, Root_Folders, 'Extra')):
+                    if dyld_map.endswith("_Dyld_Mapping.json"):
+                        with open(os.path.join(self.Extract_Path, Root_Folders, 'Extra', dyld_map), 'r') as f:
+                            data = json.load(f)
+                            Target_Dyld.extend(extract_file_paths(data))
 
+            elif Target.lower() == 'change':
+                if Target_Compare_IOS is not None:
+                    for new_root_folder in os.listdir(self.Extract_Path):
+                        if new_root_folder.split("_")[0] == Target_Compare_IOS:
+                            temp = new_root_folder
+                else:
+                    temp = Root_Folders
+                print(os.path.join(self.Extract_Path, temp, 'Extra', 'IPSW_Changes.json'))
+                with open(os.path.join(self.Extract_Path, temp, 'Extra', 'IPSW_Changes.json'), 'r') as f:
+                    for line in f:
+                        line = line.strip()
+                        if line:
+                            Target_Dyld.append(json.loads(line)["path"])
+            else:
+                self.Console_Print('[Binary] Please choose one Target options [All, Change]')
+                return
+
+            for x in range(len(Target_Dyld)):
+                self.Console_Print(f'[Binary] Running IPSW Macho Strings on {Target_Dyld[x]} on {x}/{len(Target_Dyld)}')
+                with open(os.path.join(self.Extract_Path, Root_Folders, 'Extra', 'Dyld Cache',Target_Dyld[x].replace('.dylib', '_strings.txt')), 'w') as f:
+                    image_path = '/' + Target_Dyld[x].lstrip('/')
+                    subprocess.run([
+                        'ipsw', 'dyld', 'macho',
+                        os.path.join(Dyld_Cache, 'System', 'Library', 'Caches', 'com.apple.dyld', 'dyld_shared_cache_arm64e'),
+                        image_path,
+                        '--strings'
+                    ], stdout=f, stderr=subprocess.DEVNULL, check=True)
+            self.Console_Print(f'[Binary] Completed IPSW Strings {len(Target_Dyld)}/{len(Target_Dyld)}')
+            self.Console_Print('-------------------------------------------------------')
+            for x in range(len(Target_Dyld)):
+                self.Console_Print(f'[Binary] Running IPSW Macho Symbols on {Target_Dyld[x]} on {x}/{len(Target_Dyld)}')
+                with open(os.path.join(self.Extract_Path, Root_Folders, 'Extra', 'Dyld Cache',Target_Dyld[x].replace('.dylib', '_Symbols.txt')), 'w') as f:
+                    image_path = '/' + Target_Dyld[x].lstrip('/')
+                    subprocess.run([
+                        'ipsw', 'dyld', 'macho',
+                        os.path.join(Dyld_Cache, 'System', 'Library', 'Caches', 'com.apple.dyld', 'dyld_shared_cache_arm64e'),
+                        image_path,
+                        '--symbols'
+                    ], stdout=f, stderr=subprocess.DEVNULL, check=True)
+            self.Console_Print(f'[Binary] Completed IPSW Symbols {len(Target_Dyld)}/{len(Target_Dyld)}')
+            self.Console_Print('-------------------------------------------------------')
+            for x in range(len(Target_Dyld)):
+                self.Console_Print(f'[Binary] Running IPSW Macho Symbols on {Target_Dyld[x]} on {x}/{len(Target_Dyld)}')
+                with open(os.path.join(self.Extract_Path, Root_Folders, 'Extra', 'Dyld Cache',Target_Dyld[x].replace('.dylib', '_Starts.txt')), 'w') as f:
+                    image_path = '/' + Target_Dyld[x].lstrip('/')
+                    subprocess.run([
+                        'ipsw', 'dyld', 'macho',
+                        os.path.join(Dyld_Cache, 'System', 'Library', 'Caches', 'com.apple.dyld', 'dyld_shared_cache_arm64e'),
+                        image_path,
+                        '--starts'
+                    ], stdout=f, stderr=subprocess.DEVNULL, check=True)
+            self.Console_Print(f'[Binary] Completed IPSW Starts {len(Target_Dyld)}/{len(Target_Dyld)}')
+            self.Console_Print('-------------------------------------------------------')
+            for x in range(len(Target_Dyld)):
+                self.Console_Print(f'[Binary] Running IPSW Imports on {Target_Dyld[x]} on {x}/{len(Target_Dyld)}')
+                with open(os.path.join(self.Extract_Path, Root_Folders, 'Extra', 'Dyld Cache',Target_Dyld[x].replace('.dylib', '_imports.txt')), 'w') as f:
+                    image_path = '/' + Target_Dyld[x].lstrip('/')
+                    subprocess.run([
+                        'ipsw', 'dyld', 'imports',
+                        os.path.join(Dyld_Cache, 'System', 'Library', 'Caches', 'com.apple.dyld', 'dyld_shared_cache_arm64e'),
+                        image_path
+                    ], stdout=f, stderr=subprocess.DEVNULL, check=True)
+            self.Console_Print(f'[Binary] Completed IPSW Imports {len(Target_Dyld)}/{len(Target_Dyld)}')
